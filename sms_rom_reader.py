898b55db7e96e652f59dbd682964968ab46780d3
André L. C. Moreira <andrelcmoreira@disroot.org>
2023-07-24 18:19:39 -0300

    adding parse_args and main functions

--- sms_rom_reader.py
+++ sms_rom_reader.py
@@ -1,4 +1,6 @@
+from argparse import ArgumentParser
 from enum import Enum
+from sys import argv
 
 class Offsets(Enum):
     TMR_SEGA = 0x7ff0
@@ -8,3 +10,26 @@ class Offsets(Enum):
     VERSION = 0x7ffe
     REGION_CODE = 0x7fff
     ROM_SIZE = 0x7fff
+
+def main(rom_file):
+    with open(rom_file, 'rb') as f:
+        pass
+
+def parse_args():
+    parser = ArgumentParser(prog=argv[0])
+
+    parser.add_argument('-r', '--rom-file', metavar='file',
+                        help='ROM file')
+
+    # no arguments provided
+    if len(argv) == 1:
+        parser.print_help()
+        return None
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    if args:
+        main(args.rom_file)
