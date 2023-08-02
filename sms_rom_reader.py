c65d358e3488df6e50a054b2257b41e01b9759c5
André L. C. Moreira <andrelcmoreira@disroot.org>
2023-08-01 21:51:12 -0300

    adding rom size and region code constants

--- sms_rom_reader.py
+++ sms_rom_reader.py
@@ -11,6 +11,24 @@ class Offsets(Enum):
     REGION_CODE = 0x7fff
     ROM_SIZE = 0x7fff
 
+class RomSize(Enum):
+    SIZE_8KB = 0xa
+    SIZE_16KB = 0xb
+    SIZE_32KB = 0xc
+    SIZE_48KB = 0xd
+    SIZE_64KB = 0xe
+    SIZE_128KB = 0xf
+    SIZE_256KB = 0x0
+    SIZE_512KB = 0x1
+    SIZE_1MB = 0x2
+
+class RegionCode(Enum):
+    SMS_JAPAN = 3
+    SMS_EXPORT = 4
+    GG_JAPAN = 5
+    GG_EXPORT = 6
+    GG_INTERNATIONAL = 7
+
 class Lengths(Enum):
     TMR_SEGA = 8
     RESERVED_SPACE = 2
