14ad83b82089f517cbd35d27fb9c15126cb3c70f
andré <andrelcmoreira@disroot.org>
2023-11-26 18:37:35 -0300

    fix_checksum: minor refactoring

--- fix_checksum.py
+++ fix_checksum.py
@@ -5,35 +5,34 @@ from sms.constants import Offsets, Lengths
 
 
 def fix_checksum(rom_file):
-    print('[*] loading rom...', end='')
-
-    with open(rom_file, 'r+b') as f:
-        data = f.read()
-        print('OK!')
-
-        print('[*] calculating checksum...', end='')
-        cksum = ChecksumCalc.calculate(data)
-        assert int.from_bytes(cksum) > 0, 'fail to compute the rom checksum!'
-        print('OK!')
-
-        print('[*] patching rom...', end='')
-        assert len(data) > Offsets.CHECKSUM.value, \
-            'the specified rom file has an invalid size!'
-        f.seek(Offsets.CHECKSUM.value)
-        f.write(cksum)
-        print('OK!')
-
-
-def main():
     try:
-        fix_checksum(argv[1])
+        print('[*] loading rom...', end='')
+
+        with open(rom_file, 'r+b') as f:
+            data = f.read()
+            print('OK!')
+
+            print('[*] calculating checksum...', end='')
+            cksum = ChecksumCalc.calculate(data)
+            assert int.from_bytes(cksum) > 0, \
+                'fail to compute the rom checksum!'
+            print('OK!')
+
+            print('[*] patching rom...', end='')
+            assert len(data) > Offsets.CHECKSUM.value, \
+                'the specified rom file has an invalid size!'
+            f.seek(Offsets.CHECKSUM.value)
+            f.write(cksum)
+            print('OK!')
+            print('[*] done!')
     except PermissionError:
         print('ERROR: permission denied!')
     except AssertionError as e:
         print('ERROR: %s' % str(e))
-    except IndexError:
-        print('usage: %s <rom_file>' % argv[0])
 
-
+    
 if __name__ == "__main__":
-    main()
+    if len(argv) > 1:
+        fix_checksum(argv[1])
+    else:
+        print('usage: %s <rom_file>' % argv[0])
